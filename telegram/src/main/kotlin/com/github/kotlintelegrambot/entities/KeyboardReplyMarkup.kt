1348f0dc8bdf7ec69190099c267ca2f81f2f3864
namjug-kim <soong2344@gmail.com>
2023-01-15 16:18:25 +0900

    Add missing properties KeyboardReplayMarkup

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/KeyboardReplyMarkup.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/KeyboardReplyMarkup.kt
@@ -6,28 +6,41 @@ import com.google.gson.annotations.SerializedName as Name
 
 data class KeyboardReplyMarkup(
     val keyboard: List<List<KeyboardButton>>,
+    @Name("input_field_placeholder") val inputFieldPlaceholder: String? = null,
     @Name("resize_keyboard") val resizeKeyboard: Boolean = false,
     @Name("one_time_keyboard") val oneTimeKeyboard: Boolean = false,
+    @Name("is_persistent") val isPersistent: Boolean = false,
     val selective: Boolean? = null
 ) : ReplyMarkup {
 
     constructor(
         vararg keyboard: KeyboardButton,
+        inputFieldPlaceholder: String? = null,
         resizeKeyboard: Boolean = false,
         oneTimeKeyboard: Boolean = false,
+        isPersistent: Boolean = false,
         selective: Boolean? = null
-    ) : this(listOf(keyboard.toList()), resizeKeyboard, oneTimeKeyboard, selective)
+    ) : this(listOf(keyboard.toList()), inputFieldPlaceholder, resizeKeyboard, oneTimeKeyboard, isPersistent, selective)
 
     companion object {
         val GSON = Gson()
 
         fun createSimpleKeyboard(
             keyboard: List<List<String>>,
+            inputFieldPlaceholder: String? = null,
             resizeKeyboard: Boolean = true,
             oneTimeKeyboard: Boolean = false,
+            isPersistent: Boolean = false,
             selective: Boolean? = null
         ): KeyboardReplyMarkup {
-            return KeyboardReplyMarkup(keyboard.map { it.map { KeyboardButton(text = it) } }, resizeKeyboard, oneTimeKeyboard, selective)
+            return KeyboardReplyMarkup(
+                keyboard.map { it.map { KeyboardButton(text = it) } },
+                inputFieldPlaceholder,
+                resizeKeyboard,
+                oneTimeKeyboard,
+                isPersistent,
+                selective
+            )
         }
     }
 
